371e4937cc4a80a47b8dd3581d509f271f3e5a60
Edaly09 <edalyminthe@gmail.com>
2022-08-01 14:01:59 +0200

    les functions et le formulaire

--- form/src/App.js
+++ form/src/App.js
@@ -1,24 +1,40 @@
 import { useState } from "react"
 
 const App = () =>{
+  // ICI j'initialise tous mes states 
+
 const [email, setemail] = useState("")
 const [password, setpassword] = useState("")
-// const [rememberMe, setrememberMe] = false
-// const [emailIsValid, setemailIsValid] = false
-// const [passwordIsValid, setpasswordIsValid] = false
-// const [isSubmitted, setisSubmitted] = false
+const [rememberMe, setrememberMe] = useState(false)
+const [emailIsValid, setemailIsValid] = useState(false)
+const [passwordIsValid, setpasswordIsValid] = useState(false)
+const [isSubmitted, setisSubmitted] = useState(false)
+
+// C'est ici je vais faire toutes mes fonctions juste au dessus du return
 
 const handleEmailChange = (e) =>{
-  setemail(e.target.value)
+  const value = e.target.value
+  const regEx = new RegExp(/^[\w\.]+@([\w-]+\.)+[\w-]{2,4}$/)
+  const isValid = regEx.test(value)
+  setemail(value)
   console.log(email)
 }
 const handlepasswordChange = (e) =>{
-  setemail(e.target.value)
-  console.log(password)
+  const value = e.target.value
+  setpassword(value)
+  if(value.length > 5){
+    console.log(password)
+    setpasswordIsValid(true)
+  }
+  console.log(passwordIsValid);
+
 }
 const handleSubmit = (e)=>{
   e.preventDefault()
+
 }
+  
+
 return(
   <>
   <h1>Mon formulaire</h1>
@@ -28,7 +44,6 @@ return(
     Email:
     <input type="email" name="name" onChange={handleEmailChange}/>
   </label>
-
   <label>
     Password:
     <input type="password" name="name" onChange={handlepasswordChange} />
@@ -39,7 +54,7 @@ return(
     <input type="checkbox" name="name" />
   </label>
 
-  <input type="submit" value="Submit" />
+  <button  type="submit" value="Submit" onClick={handleSubmit}> Envoyer</button>
 
 </form>
   </>
